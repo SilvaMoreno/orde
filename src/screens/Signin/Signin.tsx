db6ec326c8aac4d9b46ff33b7034e76b813c4b46
Elton Moreno <elton989@hotmail.com>
2022-01-21 18:19:46 -0100

    signin screen

--- src/screens/Signin/Signin.tsx
+++ src/screens/Signin/Signin.tsx
@@ -1,20 +1,45 @@
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { Button } from "@src/components/Button";
 import { Input } from "@src/components/Input";
-import React from "react";
 
-import { Container } from "./Style";
+import brandImg from "@assets/brand.png";
+
+import {
+  Brand,
+  Container,
+  Content,
+  Title,
+  ForgotPasswordLable,
+  ForgotPasswordButton,
+} from "./Style";
 
 export function Signin() {
   return (
     <Container>
-      <Input
-        placeholder="Email"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
-      <Button title="Entrar" type="secondary" />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Sign In</Title>
+
+          <Input
+            placeholder="Email"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLable>Forgot your password?</ForgotPasswordLable>
+          </ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
