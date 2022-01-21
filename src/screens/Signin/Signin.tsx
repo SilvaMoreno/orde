c04eb24c4ceff76057b6fe10d92e197f6e92a3d8
Elton Moreno <elton989@hotmail.com>
2022-01-21 17:34:46 -0100

    create input component

--- src/screens/Signin/Signin.tsx
+++ src/screens/Signin/Signin.tsx
@@ -1,7 +1,18 @@
+import { Input } from "@src/components/Input";
 import React from "react";
 
 import { Container } from "./Style";
 
 export function Signin() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Input
+        placeholder="Email"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+    </Container>
+  );
 }
